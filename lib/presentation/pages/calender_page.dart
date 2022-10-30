ed977dfcd0b17c8e68293579ef870b49dbcb02f2
Bulut61 <ilkerbulut1@msn.com>
2022-10-31 00:59:07 +0100

    calendar adding events with dialog but cant sync with other events

--- lib/presentation/pages/calender_page.dart
+++ lib/presentation/pages/calender_page.dart
@@ -11,40 +11,119 @@ class CalenderPage extends StatefulWidget {
 }
 
 class _CalenderPageState extends State<CalenderPage> {
+  DateTime now = DateTime.utc(2022, 10, 15, 14, 30, 000);
+  late Map<DateTime, List<Event>> selectedEvents;
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _selectedDay = DateTime.now();
   DateTime _focusedDay = DateTime.now();
+  TextEditingController _eventController = TextEditingController();
+
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Calender"), centerTitle: true),
-        body: TableCalendar(
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: _focusedDay,
-          calendarFormat: _calendarFormat,
-          startingDayOfWeek: StartingDayOfWeek.monday,
-          calendarStyle: CalendarStyle(
-              isTodayHighlighted: true,
-              selectedDecoration: BoxDecoration(
-                color: Colors.orange,
-                shape: BoxShape.circle,
-              )),
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: ((DateTime selectDay, DateTime focusDay) {
-            setState(() {
-              _selectedDay = selectDay;
-              _focusedDay = focusDay;
-            });
-          }),
-          //availableCalendarFormats: {CalendarFormat.month: 'Month', CalendarFormat.twoWeeks: '2 weeks', CalendarFormat.week: 'Week'},
-          onFormatChanged: (format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          },
+        body: ListView(
+          children: [
+            TableCalendar(
+              eventLoader: _getEventsForDay,
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              calendarStyle: CalendarStyle(
+                  isTodayHighlighted: true,
+                  selectedDecoration: BoxDecoration(
+                    color: Colors.orange,
+                    shape: BoxShape.circle,
+                  )),
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: ((DateTime selectDay, DateTime focusDay) {
+                setState(() {
+                  _selectedDay = selectDay;
+                  _focusedDay = focusDay;
+                });
+              }),
+              //availableCalendarFormats: {CalendarFormat.month: 'Month', CalendarFormat.twoWeeks: '2 weeks', CalendarFormat.week: 'Week'},
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+            ),
+            ..._getEventsForDay(_selectedDay).map((Event event) => ListTile(
+                  title: Text(event.title),
+                )),
+            SizedBox(height: 30),
+            IconButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            title: Text("Add Event"),
+                            content: TextFormField(
+                              controller: _eventController,
+                            ),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text("cancel")),
+                              TextButton(
+                                  onPressed: () {
+                                    if (_eventController.text.isEmpty) {
+                                    } else {
+                                      if (selectedEvents[_selectedDay] != null) {
+                                        selectedEvents[_selectedDay]?.add(
+                                          Event(title: _eventController.text),
+                                        );
+                                      } else {
+                                        selectedEvents[_selectedDay] = [Event(title: _eventController.text)];
+                                        selectedEvents[now] = [Event(title: "hallo")];
+                                      }
+                                    }
+                                    _eventController.clear();
+                                    Navigator.pop(context);
+                                    setState(() {});
+                                    return;
+                                  },
+                                  child: Text("ok"))
+                            ],
+                          ));
+                },
+                icon: Icon(Icons.add))
+          ],
         )); //Container();
   }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return selectedEvents[day] ?? [];
+  }
+
+  /*List<Event> _getEvents() {
+    List<Event> events = List<Event>.empty();
+    
+    return events;
+  }*/
+}
+
+class Event {
+  String title;
+  Event({required this.title});
+
+  String toString() => this.title;
 }
